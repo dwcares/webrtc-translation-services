d42049af46758eae84b4542a81bf9c604912b6bd
Margaret Meehan <mameehan@microsoft.com>
2017-09-20 13:42:22 -0400

    debugging translation apis

--- public/translation.js
+++ public/translation.js
@@ -1,12 +1,10 @@
-var makeObject = require('xml-to-js').xmlToJs;
-
-var language = 'en';
+// var makeObject = require('xml-to-js').xmlToJs;
 var TRANSLATE_ACCESS_KEY = '076bab28b4324d8fa6b3b7d6d24b5f40';
 var SPEAK_ACCESS_KEY = '5d2c8e4deea547c58c0f38ecd67641b1';
 var from_language = 'en';
 var to_language = 'es';
 
-var sample_text = encodeURI('hello my name is Margaret and i am going to the beach!');
+var sample_text = encodeURI('hello my name is Margaret and i am going to the beach.');
 var translated_text;
 
 //TRANSLATE TEXT
@@ -15,17 +13,22 @@ var translate_request_string =
 + sample_text + '&from=' + from_language + '&to=' + to_language;
 
 var translate_request = new Request(translate_request_string, {
-	headers: new Headers({
-		'Authorization': 'Bearer ' + TRANSLATE_ACCESS_KEY
-	})
+	headers: {
+        'Ocp-Apim-Subscription-Key': TRANSLATE_ACCESS_KEY
+	}
 });
+
+console.log(translate_request);
 
 fetch(translate_request).then(function(response) { 
     //response is in XML. PARSE HERE...
-    makeObject(response, function(object) {
-      console.log(object);
-      translated_text = response;
-    })
+    // makeObject(response, function(object) {
+    //   console.log(object);
+    //   translated_text = response;
+    // })
+    console.log(response);
+}).catch(function(error){
+    console.log(error);
 });
 
 //TEXT TO SPEECH
@@ -34,10 +37,12 @@ var speak_request_string = 'https://api.microsofttranslator.com/V2/Http.svc/Spea
 
 var speak_request = new Request(speak_request_string, {
 	headers: new Headers({
-		'Authorization': 'Bearer ' + SPEAK_ACCESS_KEY
+        'Ocp-Apim-Subscription-Key': SPEAK_ACCESS_KEY
 	})
 });
-fetch(translate_request).then(function(response) { 
+fetch(speak_request).then(function(response) { 
     /* handle response */ 
     //Send this audio to webrtc..
+}).catch(function(error){
+    console.log(error);
 });

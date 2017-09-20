be81f14f723974809483e19f8c3d04c72e73ed3e
Margaret Meehan <mameehan@microsoft.com>
2017-09-19 20:10:39 -0400

    added initial js file to perform API calls for translation

---
+++ public/translation.js
@@ -0,0 +1,43 @@
+var makeObject = require('xml-to-js').xmlToJs;
+
+var language = 'en';
+var TRANSLATE_ACCESS_KEY = '076bab28b4324d8fa6b3b7d6d24b5f40';
+var SPEAK_ACCESS_KEY = '5d2c8e4deea547c58c0f38ecd67641b1';
+var from_language = 'en';
+var to_language = 'es';
+
+var sample_text = encodeURI('hello my name is Margaret and i am going to the beach!');
+var translated_text;
+
+//TRANSLATE TEXT
+var translate_request_string = 
+'http://api.microsofttranslator.com/V2/Http.svc/Translate?contentType=text/plain&appid&text=' 
++ sample_text + '&from=' + from_language + '&to=' + to_language;
+
+var translate_request = new Request(translate_request_string, {
+	headers: new Headers({
+		'Authorization': 'Bearer ' + TRANSLATE_ACCESS_KEY
+	})
+});
+
+fetch(translate_request).then(function(response) { 
+    //response is in XML. PARSE HERE...
+    makeObject(response, function(object) {
+      console.log(object);
+      translated_text = response;
+    })
+});
+
+//TEXT TO SPEECH
+var speak_request_string = 'https://api.microsofttranslator.com/V2/Http.svc/Speak?appid&text=' 
++ translated_text + '&language=' + to_language + "&format=audio/mp3";
+
+var speak_request = new Request(speak_request_string, {
+	headers: new Headers({
+		'Authorization': 'Bearer ' + SPEAK_ACCESS_KEY
+	})
+});
+fetch(translate_request).then(function(response) { 
+    /* handle response */ 
+    //Send this audio to webrtc..
+});

b82f883e38399c3dd5307ec302e4fad25f58f7cf
Rami Sayar <rami.sayar@gmail.com>
2017-09-21 10:25:33 -0700

    Merged all the api calls together.

--- public/recognizer.js
+++ public/recognizer.js
@@ -3,13 +3,13 @@
 
 // On doument load resolve the SDK dependecy
 function Initialize(onComplete) {
-    require(["Speech.Browser.Sdk"], function(SDK) {
+    require(["Speech.Browser.Sdk"], function (SDK) {
         onComplete(SDK);
     });
-  }
-  
-  // Setup the recongizer
-  function RecognizerSetup(SDK, recognitionMode, language, format, subscriptionKey) {
+}
+
+// Setup the recongizer
+function RecognizerSetup(SDK, recognitionMode, language, format, subscriptionKey) {
     var recognizerConfig = new SDK.RecognizerConfig(
         new SDK.SpeechConfig(
             new SDK.Context(
@@ -18,93 +18,88 @@ function Initialize(onComplete) {
         recognitionMode, // SDK.RecognitionMode.Interactive  (Options - Interactive/Conversation/Dictation>)
         language, // Supported laguages are specific to each recognition mode. Refer to docs.
         format); // SDK.SpeechResultFormat.Simple (Options - Simple/Detailed)
-  
+
     // Alternatively use SDK.CognitiveTokenAuthentication(fetchCallback, fetchOnExpiryCallback) for token auth
     var authentication = new SDK.CognitiveSubscriptionKeyAuthentication(subscriptionKey);
-  
+
     return SDK.CreateRecognizer(recognizerConfig, authentication);
-  }
-  
-  // Start the recognition
-  function RecognizerStart(SDK, recognizer) {
+}
+
+// Start the recognition
+function RecognizerStart(SDK, recognizer) {
     recognizer.Recognize((event) => {
         /*
          Alternative syntax for typescript devs.
          if (event instanceof SDK.RecognitionTriggeredEvent)
         */
         switch (event.Name) {
-            case "RecognitionTriggeredEvent" :
+            case "RecognitionTriggeredEvent":
                 UpdateStatus("Initializing");
                 break;
-            case "ListeningStartedEvent" :
+            case "ListeningStartedEvent":
                 UpdateStatus("Listening");
                 break;
-            case "RecognitionStartedEvent" :
+            case "RecognitionStartedEvent":
                 UpdateStatus("Listening_Recognizing");
                 break;
-            case "SpeechStartDetectedEvent" :
+            case "SpeechStartDetectedEvent":
                 UpdateStatus("Listening_DetectedSpeech_Recognizing");
                 console.log(JSON.stringify(event.Result)); // check console for other information in result
                 break;
-            case "SpeechHypothesisEvent" :
+            case "SpeechHypothesisEvent":
                 UpdateRecognizedHypothesis(event.Result.Text);
                 console.log(JSON.stringify(event.Result)); // check console for other information in result
                 break;
-            case "SpeechEndDetectedEvent" :
+            case "SpeechEndDetectedEvent":
                 OnSpeechEndDetected();
                 UpdateStatus("Processing_Adding_Final_Touches");
                 console.log(JSON.stringify(event.Result)); // check console for other information in result
                 break;
-            case "SpeechSimplePhraseEvent" :
+            case "SpeechSimplePhraseEvent":
                 UpdateRecognizedPhrase(JSON.stringify(event.Result, null, 3));
                 break;
-            case "SpeechDetailedPhraseEvent" :
+            case "SpeechDetailedPhraseEvent":
                 UpdateRecognizedPhrase(JSON.stringify(event.Result, null, 3));
                 break;
-            case "RecognitionEndedEvent" :
+            case "RecognitionEndedEvent":
                 OnComplete();
                 UpdateStatus("Idle");
                 console.log(JSON.stringify(event)); // Debug information
                 break;
         }
     })
-    .On(() => {
-        // The request succeeded. Nothing to do here.
-    },
-    (error) => {
-        console.error(error);
-    });
-  }
-  
-  // Stop the Recognition.
-  function RecognizerStop(SDK, recognizer) {
+        .On(() => {
+            // The request succeeded. Nothing to do here.
+        },
+        (error) => {
+            console.error(error);
+        });
+}
+
+// Stop the Recognition.
+function RecognizerStop(SDK, recognizer) {
     // recognizer.AudioSource.Detach(audioNodeId) can be also used here. (audioNodeId is part of ListeningStartedEvent)
     recognizer.AudioSource.TurnOff();
-  }
-  
-  
-  function StartRecognition(key, lang, onComplete) {
-      Initialize((SDK) => {
+}
+
+function StartRecognition(key, lang, onComplete) {
+    Initialize((SDK) => {
         recognizer = RecognizerSetup(SDK, SDK.RecognitionMode.Conversation, lang, SDK.SpeechResultFormat.Simple, key);
 
         onComplete(recognizer);
-      });
-  }
-  
-  function UpdateStatus(status) {
-      console.log(status);
-  }
-  
-  function UpdateRecognizedHypothesis(text) {
+    });
+}
+
+function UpdateStatus(status) {
+    console.log(status);
+}
+
+function UpdateRecognizedHypothesis(text) {
     console.log(text)
-  }
-  
-  function OnSpeechEndDetected() {
-  }
-  
-  function UpdateRecognizedPhrase(json) {
-     console.log(json);
-  }
-  
-  function OnComplete() {
-  }
+}
+
+function OnSpeechEndDetected() {
+}
+
+function OnComplete() {
+}
--- public/translation.js
+++ public/translation.js
@@ -1,11 +1,10 @@
 (function() {
   var TRANSLATE_ACCESS_KEY = '076bab28b4324d8fa6b3b7d6d24b5f40';
   var SPEAK_ACCESS_KEY = '5d2c8e4deea547c58c0f38ecd67641b1';
-  var from_language = 'en';
-  var to_language = 'es';
-  var sample_text = encodeURI('hello my name is Margaret and i am going to the beach.');
+  //var from_language = 'en';
+  //var to_language = 'es';
+  //var sample_text = encodeURI('hello my name is Margaret and i am going to the beach.');
   var auth = '';
-
 
   function getAuthToken() {
     return fetch('https://api.cognitive.microsoft.com/sts/v1.0/issueToken', { method: 'POST', headers: { 'Ocp-Apim-Subscription-Key': TRANSLATE_ACCESS_KEY }})
@@ -13,29 +12,20 @@
   };
 
   getAuthToken().then(response => auth = 'Bearer ' + response);
-
-  //TRANSLATE TEXT
-  var translate_request_string = 'https://api.microsofttranslator.com/V2/Ajax.svc/Translate?appId&contentType=text/plain&text=' + sample_text + '&from=' + from_language + '&to=' + to_language+ '&appid=' + auth;
-
-  fetch(translate_request_string)
-    .then((response) => response.text())
-    .then((response) => {
-      console.log(response);
-      create_audio(response);
-  }).catch(function(error){
-      console.log(error);
-  });
+  
+  window.translateText = (sample_text, from_language, to_language) => {
+    //TRANSLATE TEXT
+    var translate_request_string = window.encodeURI('https://api.microsofttranslator.com/V2/Ajax.svc/Translate?appId&contentType=text/plain&text=' + sample_text + '&from=' + from_language + '&to=' + to_language+ '&appid=' + auth);
+  
+    return fetch(translate_request_string)
+      .then((response) => response.json());
+  };
 
   //TEXT TO SPEECH
-  function create_audio(translated_text){
+  window.create_audio = (translated_text, from_language, to_language) => {
       var speak_request_string = 'https://api.microsofttranslator.com/V2/Ajax.svc/Speak?text=' 
-      + encodeURI(translated_text) + '&language=' + to_language + "&format=audio/mp3" + '&appid=' + auth;
+      + encodeURI(translated_text) + '&language=' + to_language + "&format=audio/mp3" + '&appid=' + encodeURI(auth);
       
-      fetch(speak_request_string)
-          .then((response) => {
-              console.log(response);
-      }).catch(function(error){
-          console.log(error);
-      });
+      return fetch(speak_request_string).then((response) => response.json());
   }
 })();
